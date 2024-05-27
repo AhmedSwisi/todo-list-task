5e83c3e104a57390b452778fdd6ef3644f146c3a
Ahmed Swisi <ahmedswisi2014@gmail.com>
2024-05-28 01:41:10 +0300

    feat: Added tasks table

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,7 @@
 # app/__init__.py
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from .config import Config
-from .extensions import db, migrate, cors
+from .extensions import db, migrate
 from flask_cors import CORS
 
 
@@ -12,14 +10,14 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
     migrate.init_app(app, db)
+    db.init_app(app)
     CORS(app, resources={r"/*": {"origins": "*"}})
     app.config['CORS_HEADERS'] = 'Content-Type'
 
     with app.app_context():
         from . import routes, models
-        db.create_all()
+        # db.create_all()
 
     return app
 
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,5 @@
 # app/routes.py
 from flask import render_template, redirect, url_for
-from . import create_app
 from flask import current_app as app
 
 
