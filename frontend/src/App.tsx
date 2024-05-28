596592b82cad74c10d33386633d955beb103525e
Ahmed Swisi <ahmedswisi2014@gmail.com>
2024-05-28 18:24:40 +0300

    feat: Added update task endpoint frontend and backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { useQueryClient } from '@tanstack/react-query'
 import './App.css'
-import { Task, useAddTask, useGetTasks } from './api/tasks'
+import { Task, useAddTask, useGetTasks, useUpdateTask } from './api/tasks'
 import { Button } from './components/ui/button'
 
 
 function App() {
   const tasks = useGetTasks()
-  const mutation = useAddTask()
+  const addMutation = useAddTask()
+  const updateMutation = useUpdateTask()
   const queryClient = useQueryClient()
   return (
     <div>
@@ -14,12 +15,13 @@ function App() {
         <ul>{tasks.data?.map((task:Task) => <li key={task.id}>{task.title}</li>)}</ul>
       </div>
       <div>
-        <Button onClick={() => mutation.mutate({
+        <Button onClick={() => addMutation.mutate({
           title:'React Task 2',
           description:"React Description",
           status:"Pending",
           user_id:2
         })}>Add Task</Button>
+        <Button onClick={() => updateMutation.mutate({id:24,status:"Complete"})}>Update Task</Button>
       </div>
     </div>
   )
