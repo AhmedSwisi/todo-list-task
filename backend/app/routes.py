452a6cc0ef9252af17843104dbfc61d9329fdc29
Ahmed Swisi <ahmedswisi2014@gmail.com>
2024-05-28 17:49:45 +0300

    Added post task endpoint Frontend and Backend

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,11 @@
 # app/routes.py
-from flask import render_template, redirect, url_for, jsonify
+from flask import render_template, redirect, url_for, jsonify, request
 from flask import current_app as app
 from flask import Blueprint
 from flasgger import swag_from
 from .models import Task
+from .extensions import db
+from sqlalchemy.exc import SQLAlchemyError
 
 tasks = Blueprint('hello', __name__)
 
@@ -22,3 +24,33 @@ def read_all_tasks():
     tasks = Task.query.all()
     tasks_dict = [task.to_dict() for task in tasks]
     return jsonify(tasks_dict)
+
+@tasks.route('/tasks',methods=['POST'])
+@swag_from({
+    'responses': {
+        200: {
+            'description': 'returns all tasks',
+            'examples': {
+                'text/plain': 'Hello, From Flask Debug test resolve!'
+            }
+        }
+    }
+})
+def add_task():
+    try:
+        title = request.json['title']
+        description = request.json['description']
+        status = 'Pending'
+        user_id=request.json['user_id']
+        new_task = Task(title=title,description=description,status=status,user_id=user_id)
+        db.session.add(new_task)
+        db.session.commit()
+        return jsonify({'message':'Added task successfully',
+                        'title':title,
+                        'description':description,
+                        'status':status,
+                        'user_id':user_id
+                        }),201
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        return error
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,27 @@
 import { useQueryClient } from '@tanstack/react-query'
 import './App.css'
-import { Task, useGetTasks } from './api/tasks'
+import { Task, useAddTask, useGetTasks } from './api/tasks'
+import { Button } from './components/ui/button'
 
 
 function App() {
   const tasks = useGetTasks()
+  const mutation = useAddTask()
   const queryClient = useQueryClient()
   return (
-  
+    <div>
       <div>
         <ul>{tasks.data?.map((task:Task) => <li key={task.id}>{task.title}</li>)}</ul>
       </div>
-    
+      <div>
+        <Button onClick={() => mutation.mutate({
+          title:'React Task 2',
+          description:"React Description",
+          status:"Pending",
+          user_id:2
+        })}>Add Task</Button>
+      </div>
+    </div>
   )
 }
 
